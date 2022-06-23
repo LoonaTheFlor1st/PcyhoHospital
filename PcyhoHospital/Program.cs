824dc1ab429f87e1a4b89e6154953e33d7b1001c
Козлов Евгений <wotprodamakk@yandex.ru>
2022-06-23 12:41:15 +0300

    Подсоединение бд

--- PcyhoHospital/Program.cs
+++ PcyhoHospital/Program.cs
@@ -1,16 +1,34 @@
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.EntityFrameworkCore;
-using PcyhoHospital;
 using PcyhoHospital.Models;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
+var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddDbContext<WorkbazeContext>(
-    options =>
-        options.UseSqlServer("Server=LAB4-08;Database=Workbaze;Trusted_Connection=True;")
-    );
-await builder.Build().RunAsync();
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddDbContext<WorkbazeContext>(options =>
+    options.UseSqlServer("Server=LAB4-08;Database=Workbaze;Trusted_Connection=True;")
+);
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
+
